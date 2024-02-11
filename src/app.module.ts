6874be05ba0ef261b321df812d3721451b8ac52a
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-02-12 03:03:56 +1100

    feat: added minimal repoducable example for nestjs/bull bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
+import { AudioModule } from './audio/audio.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: 'redis://@redis:6379/?family=4',
+    }),
+    AudioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
